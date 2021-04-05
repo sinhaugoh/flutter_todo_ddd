df6bf7c4921187375462bcfb3c028fb210be6920
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-04-06 01:02:30 +0800

    implemented helper method to get the value or crash the ValueObjects

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,12 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_todo_ddd/domain/core/errors.dart';
 import 'failures.dart';
 
 @immutable
 abstract class ValueObject<T> extends Equatable{
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    //id = identity - same as writing (right) => right
+    return value.fold((l) => throw UnexpectedValueError(l), id);
+  }
 
   bool isValid() => value.isRight();
 
