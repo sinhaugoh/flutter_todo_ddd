38da6eb86bf455cd0194998d963554e349024781
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-04-05 10:20:52 +0800

    validate email and password field

---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'failures.dart';
+
+@immutable
+abstract class ValueObject<T> extends Equatable{
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  List<Object> get props => [value];
+
+  @override
+  bool get stringify => true;
+
+  @override
+  String toString() => 'Value($value)';
+}
